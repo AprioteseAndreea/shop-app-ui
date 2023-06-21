5ac41ce3ff7209272a67adfaec040e6ace597444
AprioteseAndreea <79917865+AprioteseAndreea@users.noreply.github.com>
2023-06-21 15:15:22 +0300

    Added nav bar

--- src/main/java/com/torm/demo/beans/DashboardBean.java
+++ src/main/java/com/torm/demo/beans/DashboardBean.java
@@ -2,16 +2,21 @@ package com.torm.demo.beans;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.context.ExternalContext;
+import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @Named("dashboardBean")
 @SessionScoped
+@Getter
+@Setter
 public class DashboardBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -23,6 +28,7 @@ public class DashboardBean implements Serializable {
     DashboardBean()
     {
     }
+    static int ceva = 0;
 
     @PostConstruct
     void init()
@@ -33,9 +39,11 @@ public class DashboardBean implements Serializable {
         this.products.add("Jacket Nebunie 3");
     }
 
-//    public void itemClicked(String s)
-//    {
-//        this.products.add("Jacket Nebunie 4");
-//    }
+    public void itemClicked() throws IOException {
+        this.products.add("Jacket Nebunie 4");
+//        FacesContext facesContext = FacesContext.getCurrentInstance();
+//        ExternalContext ec = facesContext.getExternalContext();
+//        ec.redirect("register.jsp");
+    }
 
 }
